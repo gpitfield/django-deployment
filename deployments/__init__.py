835d93758607efc8f8cfa7e17b6034a41dcd22ef
Geoff Pitfield <geoff@pitfield.com>
2015-03-23 17:37:52 -0400

    fix deployments

--- deployments/__init__.py
+++ deployments/__init__.py
@@ -2,8 +2,8 @@ import os, sys
 import urllib
 from boto.ec2.connection import EC2Connection
 
-def environment():
-	from django.conf import settings
+def environment(aws_access_key, aws_secret_access_key):
+	# from django.conf import settings
 	ENVIRONMENT = os.environ.get('ENVIRONMENT')
 	IS_TEST = any([el in sys.argv for el in ('test', 'deploy')])
 	IS_DEVELOPMENT = ENVIRONMENT == 'dev' and not IS_TEST
@@ -15,6 +15,7 @@ def environment():
 		this_instance_id = urllib.urlopen('http://169.254.169.254/latest/meta-data/instance-id').read()
 	except IOError:
 		return None
-	ec2_conn = EC2Connection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
+	# ec2_conn = EC2Connection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
+	ec2_conn = EC2Connection(aws_access_key, aws_secret_access_key)
 	this_instance = ec2_conn.get_all_instances(this_instance_id)[0].instances[0]
 	return this_instance.tags.get('environment', None)
