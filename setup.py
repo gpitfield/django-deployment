485df42f3c5ecc0ee203fb136e047a5357a232d2
Geoff Pitfield <geoff@pitfield.com>
2015-10-05 21:09:00 -0700

    include migrations, hopefully

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-deployments',
     version='0.1',
-    packages=['deployments'],
+    packages=['deployments', 'deployments.migrations'],
     install_requires=[
         'Django>=1.7',
         'boto==2.36.0',
